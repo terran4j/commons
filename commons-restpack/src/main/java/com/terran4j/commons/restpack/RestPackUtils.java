38c2d1945b878a0a6d34a91b038afb88e8a5f307
terran4j <terran4j@oschina.net>
2017-11-11 21:32:21 +0800

    支持用 @RestPackIgnore 过滤返回字段。

--- commons-restpack/src/main/java/com/terran4j/commons/restpack/RestPackUtils.java
+++ commons-restpack/src/main/java/com/terran4j/commons/restpack/RestPackUtils.java
@@ -2,13 +2,19 @@ package com.terran4j.commons.restpack;
 
 import com.terran4j.commons.util.Beans;
 import com.terran4j.commons.util.Classes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class RestPackUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(RestPackUtils.class);
 
     private static Set<Class<?>> basicClasses = new HashSet<Class<?>>();
 
@@ -23,8 +29,28 @@ public class RestPackUtils {
         basicClasses.add(double.class);
     }
 
+    private static Set<Class<?>> javaClasses = new HashSet<Class<?>>();
+
+    static {
+        javaClasses.add(Boolean.class);
+        javaClasses.add(Byte.class);
+        javaClasses.add(Character.class);
+        javaClasses.add(Short.class);
+        javaClasses.add(Integer.class);
+        javaClasses.add(Long.class);
+        javaClasses.add(Float.class);
+        javaClasses.add(Double.class);
+        javaClasses.add(String.class);
+        javaClasses.add(Date.class);
+        javaClasses.add(Class.class);
+    }
+
     public static final boolean isBasicType(Class<?> clazz) {
         return basicClasses.contains(clazz);
+    }
+
+    public static final boolean isJavaType(Class<?> clazz) {
+        return javaClasses.contains(clazz);
     }
 
     public static void clearIgnoreFields(Object bean) {
@@ -33,24 +59,78 @@ public class RestPackUtils {
         }
 
         Class<?> beanClass = bean.getClass();
-        Field[] ignoreFields = Classes.getFields(RestPackIgnore.class, beanClass);
-        if (ignoreFields == null || ignoreFields.length == 0) {
+        if (isJavaType(beanClass)) {
             return;
         }
 
-        for (Field ignoreField : ignoreFields) {
-            String fieldName = ignoreField.getName();
-            Class<?> fieldType = ignoreField.getType();
-            if (isBasicType(fieldType)) {
-                String msg = String.format("@%s 不允许修饰在基本类型字段上\n字段： %s %s \n类：%s",
-                        RestPackIgnore.class.getSimpleName(), fieldType.getSimpleName(), fieldName,
-                        beanClass.getName());
-                throw new RuntimeException(msg);
+        // 对于集合类型，要处理每一个子元素对象。
+        if (bean instanceof Collection) {
+            Collection collection = (Collection) bean;
+            for (Object element : collection) {
+                clearIgnoreFields(element);
             }
+            return;
+        }
+
+        // 对于 Map 类型，要处理每一个值对象。
+        if (bean instanceof Map) {
+            Map map = (Map) bean;
+            clearIgnoreFields(map.values());
+            return;
+        }
+
+        // 处理每个属性字段。
+        PropertyDescriptor[] propertyDescriptors = BeanUtils.getPropertyDescriptors(beanClass);
+        if (propertyDescriptors == null || propertyDescriptors.length == 0) {
+            return;
+        }
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            String fieldName = propertyDescriptor.getName();
+
+            // 必须满足 JavaBean 规范,要有 setter / getter 方法。
+            Method getMethod = propertyDescriptor.getReadMethod();
+            Method setMethod = propertyDescriptor.getWriteMethod();
+            if (getMethod == null || setMethod == null) {
+                continue;
+            }
+
+            Field field = null;
             try {
-                Beans.setFieldValue(bean, fieldName, null);
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                throw new RuntimeException("清除 RestPackIgnore 字段值出错： " + e.getMessage(), e);
+                field = beanClass.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+                // ignore it.
+            }
+            if (field == null) {
+                if (log.isInfoEnabled()) {
+                    log.info("在类 {} 中找不到字段：{} ", beanClass.getName(), fieldName);
+                }
+                continue;
+            }
+
+            RestPackIgnore restPackIgnore = field.getAnnotation(RestPackIgnore.class);
+            if (restPackIgnore != null) {
+                Class<?> fieldType = field.getType();
+                if (isBasicType(fieldType)) {
+                    String msg = String.format("@%s 不允许修饰在基本类型字段上\n字段： %s %s \n类：%s",
+                            RestPackIgnore.class.getSimpleName(), fieldType.getSimpleName(), fieldName,
+                            beanClass.getName());
+                    throw new RuntimeException(msg);
+                }
+                try {
+                    Beans.setFieldValue(bean, fieldName, null);
+                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                    throw new RuntimeException("清除 RestPackIgnore 字段值出错： " + e.getMessage(), e);
+                }
+            } else {
+                Object fieldValue = null;
+                try {
+                    fieldValue = getMethod.invoke(bean);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    String msg = String.format("读取字段[ %s#%s ]的值出错: %s",
+                            beanClass.getName(), fieldName, e.getMessage());
+                    throw new RuntimeException(msg);
+                }
+                clearIgnoreFields(fieldValue);
             }
         }
     }
--- commons-restpack/src/test/java/com/terran4j/test/restpack/RestPackTest.java
+++ commons-restpack/src/test/java/com/terran4j/test/restpack/RestPackTest.java
@@ -9,6 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RestPackTest {
 
@@ -19,6 +24,13 @@ public class RestPackTest {
         @RestPackIgnore
         private Boolean value = true;
 
+        public WrapTypeBean() {
+        }
+
+        public WrapTypeBean(Boolean value) {
+            this.value = value;
+        }
+
         public Boolean getValue() {
             return value;
         }
@@ -27,6 +39,15 @@ public class RestPackTest {
             this.value = value;
         }
 
+    }
+
+    @Test
+    public void testClearIgnoreFieldOfWrapType() throws Exception {
+        log.info("testClearIgnoreFieldOfWrapType");
+        WrapTypeBean bean = new WrapTypeBean();
+        Assert.assertTrue(bean.getValue());
+        RestPackUtils.clearIgnoreFields(bean);
+        Assert.assertNull(bean.getValue());
     }
 
     public static class BasicTypeBean {
@@ -45,15 +66,6 @@ public class RestPackTest {
     }
 
     @Test
-    public void testClearIgnoreFieldOfWrapType() throws Exception {
-        log.info("testClearIgnoreFieldOfWrapType");
-        WrapTypeBean bean = new WrapTypeBean();
-        Assert.assertTrue(bean.getValue());
-        RestPackUtils.clearIgnoreFields(bean);
-        Assert.assertNull(bean.getValue());
-    }
-
-    @Test
     public void testClearIgnoreFieldOfBasicType() throws Exception {
         log.info("testClearIgnoreFieldOfBasicType");
         BasicTypeBean bean = new BasicTypeBean();
@@ -65,4 +77,82 @@ public class RestPackTest {
             Assert.assertTrue(e.getMessage().contains("不允许修饰在基本类型字段上"));
         }
     }
+
+
+    public static class ListBean {
+
+        @RestPackIgnore
+        private Integer value = 1;
+
+        private List<WrapTypeBean> beans = new ArrayList<>();
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+        public List<WrapTypeBean> getBeans() {
+            return beans;
+        }
+
+        public void setBeans(List<WrapTypeBean> beans) {
+            this.beans = beans;
+        }
+    }
+
+    @Test
+    public void testClearIgnoreFieldOfList() throws Exception {
+        log.info("testClearIgnoreFieldOfList begin.");
+        ListBean bean = new ListBean();
+        bean.getBeans().add(new WrapTypeBean(true));
+        bean.getBeans().add(new WrapTypeBean(false));
+        RestPackUtils.clearIgnoreFields(bean);
+        Assert.assertNull(bean.getValue());
+        Assert.assertEquals(2, bean.getBeans().size());
+        Assert.assertNull(bean.getBeans().get(0).getValue());
+        Assert.assertNull(bean.getBeans().get(1).getValue());
+        log.info("testClearIgnoreFieldOfList end.");
+    }
+
+    public static class MapBean {
+
+        @RestPackIgnore
+        private Integer value = 1;
+
+        private Map<String, WrapTypeBean> beans = new HashMap<>();
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+        public Map<String, WrapTypeBean> getBeans() {
+            return beans;
+        }
+
+        public void setBeans(Map<String, WrapTypeBean> beans) {
+            this.beans = beans;
+        }
+    }
+
+    @Test
+    public void testClearIgnoreFieldOfMap() throws Exception {
+        log.info("testClearIgnoreFieldOfMap begin.");
+        MapBean bean = new MapBean();
+        bean.getBeans().put("a", new WrapTypeBean(true));
+        bean.getBeans().put("b", new WrapTypeBean(false));
+        RestPackUtils.clearIgnoreFields(bean);
+        Assert.assertNull(bean.getValue());
+        Assert.assertEquals(2, bean.getBeans().size());
+        Assert.assertNull(bean.getBeans().get("a").getValue());
+        Assert.assertNull(bean.getBeans().get("b").getValue());
+        log.info("testClearIgnoreFieldOfMap end.");
+    }
+
 }
