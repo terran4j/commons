6aa567f17b890b642585abfdec504ba082a1e1f0
terran4j <terran4j@oschina.net>
2018-02-10 11:28:03 +0800

    支持从类上找 @ApiComment， 并且找上一层的 seeClass.

--- commons-api2doc/src/main/java/com/terran4j/commons/api2doc/impl/ApiCommentUtils.java
+++ commons-api2doc/src/main/java/com/terran4j/commons/api2doc/impl/ApiCommentUtils.java
@@ -6,6 +6,9 @@ import com.terran4j.commons.util.Classes;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class ApiCommentUtils {
 
@@ -83,8 +86,15 @@ public class ApiCommentUtils {
         return null;
     }
 
+    /**
+     * 获取可参照的 @ApiComment 注解对象。
+     * @param apiComment
+     * @param defaultSeeClass
+     * @param defaultSeeField
+     * @return
+     */
     private static ApiComment getSeeApiComment(
-            ApiComment apiComment, Class<?> defaultSeeClass, String defaultName) {
+            ApiComment apiComment, Class<?> defaultSeeClass, String defaultSeeField) {
 
         Class<?> seeClass = null;
         if (apiComment != null ) {
@@ -97,24 +107,54 @@ public class ApiCommentUtils {
             return null;
         }
 
-        String name = null;
+        String seeField = null;
         if (apiComment != null) {
-            name = apiComment.seeField();
+            seeField = apiComment.seeField();
         }
-        if (StringUtils.isEmpty(name)) {
-            name = defaultName;
+        if (StringUtils.isEmpty(seeField)) {
+            seeField = defaultSeeField;
         }
-        if (StringUtils.isEmpty(name)) {
+        if (StringUtils.isEmpty(seeField)) {
             return null;
         }
 
-        Field field = Classes.getField(name, seeClass);
-        if (field == null) {
-            return null;
+        // 记录引用过的 seeClass， 用于循环引用检测。
+        List<Class<?>> path = new ArrayList<>();
+        Field field = null;
+        ApiComment seeComment = null;
+        while (seeClass != null) {
+
+            // 循环引用检测。
+            if (path.contains(seeClass)) {
+                StringBuffer sb = new StringBuffer();
+                sb.append("@ApiComment 中的 seeClass 不允许循环引用：");
+                for (int i = 0; i < path.size(); i++) {
+                    if (i > 0) {
+                        sb.append(" --> ");
+                    }
+                    sb.append(seeClass.getSimpleName());
+                }
+                throw new RuntimeException(sb.toString());
+            }
+            path.add(seeClass);
+
+            // 寻找匹配字段中的 ApiComment 。
+            field = Classes.getField(seeField, seeClass);
+            if (field != null) {
+                seeComment = field.getAnnotation(ApiComment.class);
+                if (seeComment != null) {
+                    return seeComment;
+                }
+            }
+
+            ApiComment parentApiComment = seeClass.getAnnotation(ApiComment.class);
+            if (parentApiComment == null) {
+                break;
+            }
+            seeClass = parentApiComment.seeClass();
         }
 
-        ApiComment seeComment = field.getAnnotation(ApiComment.class);
-        return seeComment;
+        return null;
     }
 
 }
---
+++ commons-api2doc/src/test/java/com/terran4j/test/commons/api2doc/ParseApiCommentOnSeeClassLoop.java
@@ -0,0 +1,120 @@
+package com.terran4j.test.commons.api2doc;
+
+import com.terran4j.commons.api2doc.annotations.Api2Doc;
+import com.terran4j.commons.api2doc.annotations.ApiComment;
+import com.terran4j.commons.api2doc.domain.ApiDocObject;
+import com.terran4j.commons.api2doc.domain.ApiFolderObject;
+import com.terran4j.commons.api2doc.domain.ApiParamObject;
+import com.terran4j.commons.api2doc.domain.ApiResultObject;
+import com.terran4j.commons.api2doc.impl.Api2DocCollector;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ParseApiCommentOnSeeClassLoop {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            ParseApiCommentOnSeeClassLoop.class);
+
+    public class Comments {
+
+        @ApiComment(value = "用户id", sample = "123")
+        private Long id;
+
+        @ApiComment(value = "用户名称", sample = "neo")
+        private Long name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Long getName() {
+            return name;
+        }
+
+        public void setName(Long name) {
+            this.name = name;
+        }
+    }
+
+    @ApiComment(seeClass = Comments.class)
+    public class User {
+
+        private Long id;
+
+        private Long name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Long getName() {
+            return name;
+        }
+
+        public void setName(Long name) {
+            this.name = name;
+        }
+    }
+
+    @Api2Doc
+    @ApiComment(seeClass = User.class)
+    @RestController
+    @RequestMapping(value = "/test/api2doc/seeClass")
+    public static class MyController {
+
+        @RequestMapping(value = "/user/{id}")
+        public User updateUser(
+                @PathVariable("id") Long id,
+                @RequestParam("name") String name) {
+            return null;
+        }
+    }
+
+    @Test
+    public void testParseApiCommentOnSeeClassLoop() throws Exception {
+        log.info("testParseApiCommentOnSeeClass");
+
+        Api2DocCollector collector = new Api2DocCollector();
+        ApiFolderObject folder = collector.toApiFolder(
+                new ParseApiCommentOnSeeClassLoop.MyController(), "myController");
+        ApiDocObject doc = folder.getDoc("updateUser");
+
+        List<ApiParamObject> params = doc.getParams();
+        Assert.assertEquals("用户id", params.get(0).getComment().toString());
+        Assert.assertEquals("123", params.get(0).getSample().toString());
+        Assert.assertEquals("用户名称", params.get(1).getComment().toString());
+        Assert.assertEquals("neo", params.get(1).getSample().toString());
+
+        ApiResultObject user = doc.getResults().get(0);
+
+        ApiResultObject userId = user.getChildren().get(0);
+        Assert.assertEquals("id", userId.getId());
+        Assert.assertEquals("用户id", userId.getComment().getValue());
+        Assert.assertEquals("123", userId.getSample().getValue());
+
+        ApiResultObject userName = user.getChildren().get(1);
+        Assert.assertEquals("name", userName.getId());
+        Assert.assertEquals("用户名称", userName.getComment().getValue());
+        Assert.assertEquals("neo", userName.getSample().getValue());
+    }
+
+}
